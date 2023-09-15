d30ab2159a21fca4161150aa99b7084bd970c42c
jgarciah-esliceu <jgarciah@esliceu.net>
2023-09-15 19:08:25 +0200

    Instalar PHPMailer

--- controllers/PaginasController.php
+++ controllers/PaginasController.php
@@ -51,7 +51,14 @@ class PaginasController {
         $router->render('/paginas/entrada');
     }
 
-    public static function contacto() {
-        echo "Desde contacto";
+    public static function contacto(Router $router) {
+
+        if($_SERVER['REQUEST_METHOD'] === 'POST') {
+            debuguear($_POST);
+        }
+        
+        $router->render('/paginas/contacto', [
+
+        ]);
     }
 }
