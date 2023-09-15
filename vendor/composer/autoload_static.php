96c42883da284a9f09e716a0165e40617bb75770
jgarciah-esliceu <jgarciah@esliceu.net>
2023-09-15 19:14:43 +0200

    Instalacion PHPMailer

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -14,6 +14,7 @@ class ComposerStaticInitf0014ba35e923cb6eed26901feb51744
         'P' => 
         array (
             'Psr\\Http\\Message\\' => 17,
+            'PHPMailer\\PHPMailer\\' => 20,
         ),
         'M' => 
         array (
@@ -39,6 +40,10 @@ class ComposerStaticInitf0014ba35e923cb6eed26901feb51744
         array (
             0 => __DIR__ . '/..' . '/psr/http-factory/src',
             1 => __DIR__ . '/..' . '/psr/http-message/src',
+        ),
+        'PHPMailer\\PHPMailer\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/phpmailer/phpmailer/src',
         ),
         'Model\\' => 
         array (
