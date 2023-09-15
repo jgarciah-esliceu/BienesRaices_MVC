d4613783f2ac072416b9fcb5fb7eff07205cf36b
jgarciah-esliceu <jgarciah@esliceu.net>
2023-09-15 18:48:07 +0200

    Crear pagina de contacto y configurar envio mails

--- controllers/PaginasController.php
+++ controllers/PaginasController.php
@@ -42,12 +42,13 @@ class PaginasController {
         ]);
     }
 
-    public static function blog() {
-        echo "Desde blog";
+    public static function blog(Router $router) {
+        
+        $router->render('/paginas/blog');
     }
 
-    public static function entrada() {
-        echo "Desde entrada";
+    public static function entrada(Router $router) {
+        $router->render('/paginas/entrada');
     }
 
     public static function contacto() {
---
+++ views/paginas/entrada.php
@@ -0,0 +1,38 @@
+<main class="contenedor seccion contenido-centrado">
+    <h1>Guía para decoracion de tu hogar</h1>
+
+    <picture>
+        <source srcset="build/img/destacada2.webp" type="image/webp">
+        <source srcset="build/img/destacada2.jpg" type="image/jpeg">
+        <img src="build/img/destacada2.jpg" alt="Imagen Casa" loading="lazy">
+    </picture>
+
+    <p class="informacion-meta">Escrito el: <span>30/12/2023</span> por: <span>Admin</span></p>
+
+    <div class="resumen-propiedad">
+
+        <p>
+            Etiam semper lacus sed quam cursus, non maximus enim vestibulum.
+            Morbi id ligula id nisi egestas gravida. Nulla varius diam quis sem
+            scelerisque, non gravida mi lacinia. Duis convallis erat ut dui
+            auctor tempor. Cras pharetra blandit nisl, at facilisis neque
+            vehicula a. Nunc non gravida justo, eget consectetur mi. Curabitur
+            vehicula ipsum vel volutpat sagittis. Nam placerat tellus a diam
+            luctus pellentesque. Proin vitae justo placerat, tempor ligula et,
+            cursus odio. Aliquam aliquet efficitur lectus, sed scelerisque purus
+            venenatis non. Nam in turpis ut eros scelerisque fringilla.
+        </p>
+
+        <p>
+            Etiam semper lacus sed quam cursus, non maximus enim vestibulum.
+            Morbi id ligula id nisi egestas gravida. Nulla varius diam quis sem
+            scelerisque, non gravida mi lacinia. Duis convallis erat ut dui
+            auctor tempor. Cras pharetra blandit nisl, at facilisis neque
+            vehicula a. Nunc non gravida justo, eget consectetur mi. Curabitur
+            vehicula ipsum vel volutpat sagittis. Nam placerat tellus a diam
+            luctus pellentesque. Proin vitae justo placerat, tempor ligula et,
+            cursus odio. Aliquam aliquet efficitur lectus, sed scelerisque purus
+            venenatis non. Nam in turpis ut eros scelerisque fringilla.
+        </p>
+    </div>
+</main>
