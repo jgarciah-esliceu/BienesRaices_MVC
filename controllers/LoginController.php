b8d2214966c43976b85284743d52ac1c9d3d8af1
jgarciah-esliceu <jgarciah@esliceu.net>
2023-09-18 23:30:23 +0200

    Proteger las rutas

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -43,6 +43,10 @@ class LoginController {
     }
 
     public static function logout() {
-        echo "Desde logout";
+        session_start();
+
+        $_SESSION = [];
+
+        header('Location: /');
     }
 }
